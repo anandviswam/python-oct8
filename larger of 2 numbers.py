20430b96cd9553eb1a0b57975d4e9552b165e497
anandviswam <anandv2028@cs.sjcetpalai.ac.in>
2024-10-15 14:34:32 +0530

    Add files via upload

---
+++ larger of 2 numbers.py
@@ -0,0 +1,14 @@
+#author:anand v
+#date:15/10/2024
+num_1=int(input("enyter the first number"))
+num_2=int(input("enter the second number"))
+if num_1 > num_2:
+    print(num_1)
+elif num_1 <= num_2:
+    print(num_2)
+else :
+    print(num_1==num_2)
+
+
+
+
