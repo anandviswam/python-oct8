007f14ee97399d5cc28d2d05304b047f8345ad05
anandviswam <anandv2028@cs.sjcetpalai.ac.in>
2024-10-15 15:52:17 +0530

    Add files via upload

---
+++ color code.py
@@ -0,0 +1,11 @@
+#author:anand v
+#date:15/10/24
+grade=input("enter the colour code")
+if grade=="r" or grade=="R":
+    print("Red")
+elif grade=="g" or grade=="G":
+    print("green")
+elif grade=="b" or grade=="B":
+    print("blue")
+else:
+    print("wrong code")
