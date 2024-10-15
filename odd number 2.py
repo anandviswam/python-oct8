700a784dce12c1e2b37cbf33de0f07041002136f
anandviswam <anandv2028@cs.sjcetpalai.ac.in>
2024-10-15 16:21:51 +0530

    Add files via upload

---
+++ odd number 2.py
@@ -0,0 +1,7 @@
+#author:anand v
+#date:15/10/24
+limit=int(input("enter a limit"))
+odd_number=1
+while odd_number<limit:
+    print(odd_number ,"\t",end="")
+    odd_number+=2
---
+++ odd numbers.py
@@ -0,0 +1,9 @@
+#author:anand v
+#date:15/10/24
+num=int(input("enter a number:"))
+f=1
+while num>0:
+    f=f*num
+    num=num-1
+    print(f)
+
