0f62398ab88373f5322bfd12396c496e1c223470
anandviswam <anandv2028@cs.sjcetpalai.ac.in>
2024-10-15 13:37:23 +0530

    Add files via upload

---
+++ tast 2_name.py
@@ -0,0 +1,11 @@
+#author:anand v
+#date:08/10/2024
+first_name=input("enter your first name")
+last_name=input("enter your last name")
+name=first_name+" "+last_name
+print(name)
+leng1=len(first_name)
+leng=len(name)
+lstname=name[leng1+1:leng]
+print(lstname)
+
