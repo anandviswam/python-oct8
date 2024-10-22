a639f635aa81688541659cf605db52eaa65ffa1b
anandviswam <anandv2028@cs.sjcetpalai.ac.in>
2024-10-22 14:21:16 +0530

    Add files via upload

---
+++ largest number.py
@@ -0,0 +1,12 @@
+#author:anand v
+#date:22/10/2024
+#aim:find the largest number
+num1=int(input("enter first number"))
+num2=int(input("enter second number"))
+num3=int(input("enter third number"))
+if(num1>num2 and num1>num3):
+    print(num1," is the largest")
+elif(num2>num2 and num2>num3):
+    print(num2," is the largest")
+elif(num3>num1 and num3>num2):
+    print(num3," is the largest")
